f9ba376262da47ff719d6878a441eba0f4556383
void-zenith <zenithrajbhandari@gmail.com>
2024-03-21 20:37:14 -0400

    implement screen 1

--- app/src/main/java/com/zenith/animation_assignment/screen/Screen1.kt
+++ app/src/main/java/com/zenith/animation_assignment/screen/Screen1.kt
@@ -1,43 +1,88 @@
 package com.zenith.animation_assignment.screen
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.*
+import androidx.compose.animation.*
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.navigation.NavController
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun Screen1(navController: NavController) {
-    var textFieldValue by remember { mutableStateOf(TextFieldValue()) }
-
+    var count by remember {
+        mutableStateOf(0)
+    }
+    var oldCount by remember {
+        mutableStateOf(count)
+    }
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Main Screen")
-        Spacer(modifier = Modifier.height(16.dp))
-        BasicTextField(
-            value = textFieldValue,
-            onValueChange = { textFieldValue = it },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            val inputText = textFieldValue.text
-            if (inputText.isNotBlank()) {
-                navController.navigate("screen2")
+        SideEffect {
+            oldCount = count
+        }
+        Row(modifier = Modifier) {
+            val countString = count.toString()
+            val oldCountString = oldCount.toString()
+            for(i in countString.indices) {
+                val oldChar = oldCountString.getOrNull(i)
+                val newChar = countString[i]
+                val char = if(oldChar == newChar) {
+                    oldCountString[i]
+                } else {
+                    countString[i]
+                }
+                AnimatedContent(
+                    targetState = char,
+                    transitionSpec = {
+
+                        if(oldCount < count){
+                            slideInVertically { it } with slideOutVertically { -it }
+
+                        }else {
+                            slideInVertically { -it } with slideOutVertically { it }
+
+                        }
+                    }
+                ) { char ->
+                    Text(
+                        text = char.toString(),
+                        softWrap = false,
+                        fontSize = 30.sp
+                    )
+                }
             }
-        }) {
-            Text("Go to Screen 3")
+        }
+
+        Button(onClick = { count++ }) {
+            Text(text = "Increment")
+        }
+        Button(onClick = {  count-- }) {
+            Text(text = "Decrement")
+        }
+        Button(onClick = {navController.navigate("main")}) {
+            Text(text = "Go Back")
         }
     }
+}
+
+
+@Composable
+private fun Expanded() {
+}
+
+@Composable
+private fun ContentIcon() {
 }
